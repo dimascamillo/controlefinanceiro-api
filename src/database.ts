3513a6d24bbb2413a8fbfa80aa87abc8d5034b5a
Dimas Camillo <dimas.camillo@sonda.com>
2023-03-06 20:12:16 -0300

    Criado migrations

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,18 @@
-import { knex as setupKnex } from "knex";
+import { knex as setupKnex, Knex } from "knex";
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
     password: '123456',
-    database: 'transactions'
+    database: 'transaction'
+  },
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
   }
-})
+}
+
+export const knex = setupKnex(config)
